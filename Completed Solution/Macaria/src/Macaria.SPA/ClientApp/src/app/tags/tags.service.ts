106e44b44b0c746bf59667e3d195c0bc819dd568
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-04-21 18:20:14 -0400

    caching

--- Completed Solution/Macaria/src/Macaria.SPA/ClientApp/src/app/tags/tags.service.ts
+++ Completed Solution/Macaria/src/Macaria.SPA/ClientApp/src/app/tags/tags.service.ts
@@ -3,13 +3,20 @@ import { HttpClient } from "@angular/common/http";
 import { constants } from "../shared/constants";
 import { Tag } from "./tag.model";
 import { Observable } from "rxjs/Observable";
+import { shareReplay, tap } from "rxjs/operators";
+import { HubClient } from "../shared/hub-client";
 
 @Injectable()
 export class TagsService {
   constructor(
     private _httpClient: HttpClient,
+    private _hubClient: HubClient
     @Inject(constants.BASE_URL) private _baseUrl: string) {
 
+    this._hubClient.messages$
+      .pipe(
+        tap(() => this._cache$ = null)
+      ).subscribe();
   }
 
   public save(options) {
@@ -18,16 +25,30 @@ export class TagsService {
   }
 
   public get(): Observable<{ tags: Array<Tag> }> {
-    return this._httpClient
-      .get<{ tags: Array<Tag> }>(`${this._baseUrl}api/tags`)
+  
+    if (!this._cache$) {
+      this._cache$ = this._get()
+        .pipe(
+          shareReplay(1)
+        );
+    }
+
+    return this._cache$;
   }
 
   public getById(options) {
-
+    
   }
 
   public remove(options) {
     return this._httpClient
       .delete<{ tags: Array<Tag> }>(`${this._baseUrl}api/tags/${options.tagId}`)
   }
+
+  private _get(): Observable<{ tags: Array<Tag> }> {  
+    return this._httpClient
+      .get<{ tags: Array<Tag> }>(`${this._baseUrl}api/tags`)
+  }
+
+  private _cache$: Observable<{ tags: Array<Tag> }>;  
 }

7022cd2e0dc89c0d1c55064365f0872477a8c1e1
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-06-16 16:46:48 -0400

    unit tests

---
+++ test/UnitTests/API/NoteUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Macaria.API.Features.Notes;
+using Macaria.API.Features.Tags;
+using Macaria.Core.Entities;
+using Macaria.Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.API
+{
+    public class NoteUnitTests
+    {
+        [Fact]
+        public async Task ShouldHanldSavNoteCommandRequest() {
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "ShouldHandleSaveNoteCommandRequest")
+                .Options;
+
+            using (var context = new AppDbContext(null, null)) {
+
+
+            }
+        }
+    }
+}
